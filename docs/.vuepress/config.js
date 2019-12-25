0a23cfeed1f27f10dd5b5bb0a6589f1e3c264f89
PingTouG <2798874773@qq.com>
2019-12-25 18:13:49 +0800

    chore(docs): 配置环境

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'VXUI',
+  description: '基于Vue开发的组件库',
+  base: '/',
+  head: [['link', { rel: 'icon', href: 'favicon.icon' }]],
+  themeConfig: {
+    nav: [
+      { text: '指南', link: '/guide' },
+      { text: 'GitHub', link: 'https://github.com/PingTouG/vxui' }
+    ]
+  }
+}
--- examples/index.js
+++ examples/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from '@/App'
-import VXUI from '../lib/vxui.js'
+import VXUI from '../packages'
 
 Vue.use(VXUI)
 
