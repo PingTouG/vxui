845da01cf11af26b804ce3d68fb3f90765e17539
PingTouG <2798874773@qq.com>
2019-12-26 09:20:42 +0800

    fix(docs): 修复图标问题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'VXUI',
   description: '基于Vue开发的组件库',
   base: '/',
-  head: [['link', { rel: 'icon', href: 'favicon.icon' }]],
+  head: [['link', { rel: 'icon', href: 'favicon.ico' }]],
   themeConfig: {
     nav: [
       { text: '指南', link: '/guide' },
