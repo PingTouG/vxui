cf6c5cebf1ec7842a4b8e50755e11c8575be1002
PingTouG <2798874773@qq.com>
2019-12-26 18:24:54 +0800

    fix(packages): 完善Button组件

--- packages/Button/src/Button.vue
+++ packages/Button/src/Button.vue
@@ -1,15 +1,27 @@
 <template>
-  <div class="x-button">Button</div>
+  <button class="x-button" :class="`x-button--${type}`">
+    <span v-if="!$slots.default">{{ text }}</span>
+    <slot />
+  </button>
 </template>
 
 <script>
 export default {
-  name: 'XButton'
+  name: 'XButton',
+  props: {
+    text: String,
+    type: {
+      type: String,
+      default: 'default',
+      validator: value =>
+        ['default', 'primary', 'success', 'info', 'warning', 'danger'].indexOf(
+          value
+        ) !== -1
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-.x-button {
-  font-size: 20px;
-}
+@import '../../styles/button.scss';
 </style>
