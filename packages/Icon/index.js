c04a861646b1fcdb7a9a510447dca45bed16c605
PingTouG <2798874773@qq.com>
2019-12-31 10:45:01 +0800

    feat(packages): 添加图标组件

---
+++ packages/Icon/index.js
@@ -0,0 +1,5 @@
+import XIcon from './src/Icon'
+
+XIcon.install = Vue => Vue.component(XIcon.name, XIcon)
+
+export default XIcon
---
+++ packages/Icon/src/Icon.vue
@@ -0,0 +1,16 @@
+<template>
+  <i class="iconfont" :class="`icon-${name}`" />
+</template>
+
+<script>
+export default {
+  name: 'XIcon',
+  props: {
+    name: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets//iconfont/iconfont.css';
+</style>
