5fe3772891a2093178a04be15abb7e5a9ce59917
PingTouG <2798874773@qq.com>
2020-01-06 16:52:09 +0800

    fix(packages:Button): 添加ButtonGroup

---
+++ packages/Button/src/ButtonGroup.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="x-button-group">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'XButtonGroup'
+}
+</script>
+
+<style lang="scss">
+$radius: 4px;
+$borderColor: rgba(255, 255, 255, 0.5);
+
+.x-button-group {
+  display: inline-block;
+  vertical-align: middle;
+
+  & > .x-button {
+    float: left;
+    position: relative;
+    border-radius: 0;
+
+    &:first-child {
+      border-top-left-radius: $radius;
+      border-bottom-left-radius: $radius;
+      border-right-color: $borderColor;
+    }
+
+    &:last-child {
+      border-top-right-radius: $radius;
+      border-bottom-right-radius: $radius;
+      border-left-color: $borderColor;
+    }
+
+    &:not(:first-child):not(:last-child) {
+      border-left-color: $borderColor;
+      border-right-color: $borderColor;
+    }
+
+    &:not(:last-child) {
+      margin-right: -1px;
+    }
+  }
+}
+</style>
---
+++ packages/ButtonGroup/index.js
@@ -0,0 +1,5 @@
+import XButtonGroup from '../Button/src/ButtonGroup'
+
+XButtonGroup.install = Vue => Vue.component(XButtonGroup.name, XButtonGroup)
+
+export default XButtonGroup
